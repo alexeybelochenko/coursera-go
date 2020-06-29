e75ce9857901aeb4339419dab7c0ea09441ba983
Alexey Belochenko <alexey.belochenko@gmail.com>
2020-06-29 15:10:48 +0300

    Complete all task at first go course

--- src/array_examples.go
+++ src/array_examples.go
@@ -1,13 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var x[5] int
-	y := [3]int {1, 2, 3}
+	//var x[5] int
+	// y := [3]int{1, 2, 3}
 	
-	for index, value range y{
-		fmt.Println("%d %d", index, value)
-	} 
+	// for index, value range y{
+	// 	fmt.Println("%d %d", index, value)
+	// } 
+
+	arr := [...]string{"a", "b", "c", "d", "e", "f", "g"}
+	s1 := arr[1:3]
+	//s2 := arr[2:5]
+	fmt.Println(s1)
 
 }
---
+++ src/slice.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	storage := make([]int, 3)
+	var input string
+	counter := 0
+	for {
+		fmt.Printf("Enter integer: ")
+		fmt.Scan(&input)
+
+		if strings.ToLower(input) == "x" {
+			break
+		}
+
+		if value, err := strconv.Atoi(input); err == nil {
+			if counter < 3 {
+				storage[counter] = value
+			} else {
+				storage = append(storage, value)
+			}
+			sorted := make([]int, len(storage))
+			copy(sorted, storage)
+			sort.Ints(sorted)
+			fmt.Println(sorted)
+			counter++
+		}
+	}
+}
